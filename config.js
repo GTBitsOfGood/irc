41b175da88d06800365ca3d22a3a23c9d9f61daf
Lincoln Anders <lincolnanders5@gmail.com>
2018-10-03 00:40:29 -0400

    Added db user account reference to .env

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 var config = {}
 
+var env = require("./.env");
+
 config.port = 5000;
 config.db_path = "hello";
-config.db_url = `mongodb://localhost:27017/${config.db_path}`
+config.db_url = `mongodb+srv://${env.username}:${env.password}@ircdb-adim8.mongodb.net/test?retryWrites=true`
 
 module.exports = config;
