57b29c1edfead71f6b1fd522aff95cd69b7b0818
Lincoln Anders <lincolnanders5@gmail.com>
2018-10-27 18:00:20 -0400

    Added global JWT secret config

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
-var config = {}
+var config = {};
 
 var env = require("./.env");
 
 config.port = 5000;
 config.db_path = "hello";
-config.db_url = `mongodb+srv://${env.username}:${env.password}@ircdb-adim8.mongodb.net/test?retryWrites=true`
+config.db_url = `mongodb+srv://${env.username}:${env.password}@ircdb-adim8.mongodb.net/test?retryWrites=true`;
+
+config.JWT_SECRET = "KLD24Fj#U_RI@JRIJi5!jdf20rk3jlk4?jfa";
 
 module.exports = config;
