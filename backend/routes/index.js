10f4ba67d5447857525fb3d218b6dc11926e4ad9
Prateek Shah <pcshah1996@gmail.com>
2018-11-11 18:10:31 -0500

    Login auth system (#2)
    * Add baseline
    
    * Add extra deps into package.json
    
    * Added db_url config item
    
    * Pointed db to config.db_url and made the mongoose options work

---
+++ backend/routes/index.js
@@ -0,0 +1,40 @@
+//
+// Header for routes/index.js
+//
+
+const express = require('express');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+const router = express.Router();
+
+router.post('/signup', passport.authenticate('signup', { session : false }) , async (req, res, next) => {
+  res.json({ 
+    message : 'Signup successful',
+    user : req.user 
+  });
+});
+
+router.post('/login', async (req, res, next) => {
+  passport.authenticate('login', async (err, user, info) => {     try {
+      if(err || !user){
+        const error = new Error('An Error occured')
+        return next(error);
+      }
+      req.login(user, { session : false }, async (error) => {
+        if( error ) return next(error)
+        //We don't want to store the sensitive information such as the
+        //user password in the token so we pick only the email and id
+        const body = { _id : user._id, email : user.email };
+        //Sign the JWT token and populate the payload with the user email and id
+        const token = jwt.sign({ user : body },'dragonsandpandas');
+        //Send back the token to the user
+        return res.json({ token });
+      });     } catch (error) {
+      return next(error);
+    }
+  })(req, res, next);
+});
+
+module.exports = router;
+
