73a8a04f6dc122dcf4d92f28332854c061f448d5
Emily Wilson <emilywilson6263@gmail.com>
2018-11-15 09:04:57 -0500

    add mobx to register component

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,29 +1,51 @@
 import React, { Component } from 'react';
 
 class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      status: ''
+    };
+    this.onRegister = this.onRegister.bind(this);
+  }
+
+  onRegister() {
+    this.props.store.RegisterModel.register(this.state.email, this.state.password, this.state.status);
+  }
+
   render() {
     return (
       <div className="login-clean">
-        <form method="post">
+        <form>
           <h2 className="sr-only">Register</h2>
           <div className="form-group">
-            <input className="form-control" type="email" name="email" placeholder="Email" onChange={(e) => this.username = e.target.value }/>
+            <input className="form-control" type="email" name="email" placeholder="Email" onChange={(e) => {
+              this.setState({ email: e.target.value})
+            }} />
           </div>
           <div className="form-group">
-            <input className="form-control" type="password" name="password" placeholder="Password" onChange={(e) => this.password = e.target.value} />
+            <input className="form-control" type="password" name="password" placeholder="Password" onChange={(e) => {
+              this.setState({password: e.target.value})
+            }} />
           </div>
           <div className="form-group">
             <span>
-              <input className="form-control" type="radio" value="admin" />
+              <input name="status" className="form-control" type="radio" value="admin" onChange={(e) => {
+                this.setState({status: e.target.value})
+              }}/>
               <label>Admin</label>
             </span>
             <span>
-              <input className="form-control" type="radio" value="user" />
+              <input name="status" className="form-control" type="radio" value="user" onChange={(e) => {
+                this.setState({status: e.target.value})
+              }} />
               <label>User</label>
             </span>
           </div>
           <div className="form-group">
-            <button className="btn btn-primary btn-block" type="submit">Register User</button>
+            <button className="btn btn-primary btn-block" onClick={this.onRegister}>Register User</button>
           </div>
         </form>
       </div>
--- frontend/src/models/RootStore.js
+++ frontend/src/models/RootStore.js
@@ -1,10 +1,12 @@
 import StoreModel from './StoreModel';
 import ItemModel from './ItemModel';
 import ShoppingCartModel from './ShoppingCartModel';
+import RegisterModel from './RegisterModel';
 
 export default class RootStore {
   constructor() {
     this.StoreModel = new StoreModel();
     this.ShoppingCartModel = new ShoppingCartModel();
+    this.RegisterModel = new RegisterModel();
   }
 }
