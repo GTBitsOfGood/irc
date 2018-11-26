d3981c93eb31d862d93812ec01d935f50e29ecb7
kevintian <kevintian@gatech.edu>
2018-11-25 21:53:37 -0500

    added route for updating items

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Transaction = require('../model/transaction.js');
-const TransactionItem = require('../model/transactionItem.js')
+const { ShopItem, VolunteerItem } = require('../model/transactionItem.js')
 
 
 router.get('/client', async (req, res, next) => {
@@ -26,18 +26,75 @@ router.get('/authorizedUser', async (req, res, next) => {
     }
 });
 
-router.get('/items', async (req, res, next) => {
-    try {
-        const allItemsByVersion = await TransactionItem.find({
-            revisionNumber: req.query.revisionNumber
-        });
-        res.json(allItemsByVersion);
-    } catch (err) {
-        next(err);
+router.get('/getShopItems', async (req, res, next) => {
+    if (req.query.revisionNumber) {
+        try {
+            const allItemsByVersion = await ShopItem.find({
+                revisionNumber: req.query.revisionNumber
+            });
+            res.json(allItemsByVersion);
+        } catch (err) {
+            next(err);
+        }
+    } else {
+        try {
+            const allItemsByMostRecentVersion = await ShopItem.aggregate(
+                [
+                    // Group by revisionNumber, assembling an array of docs with each distinct value.
+                    {
+                        $group: {
+                            _id: '$revisionNumber',
+                            items: { $push: '$$ROOT' }
+                        }
+                    },
+                    // Sort the groups by _id descending to put the max revisionNumber group first.
+                    { $sort: { revisionNumber: -1 } },
+                    // Return just the first (max revisionNumber) group of docs.
+                    { $limit: 1 }
+                ]
+            );
+            res.json(allItemsByMostRecentVersion);
+        } catch (err) {
+            next(err);
+        }
     }
 });
 
-router.get('/', async (req, res, next) => {
+router.get('/getVolunteerItems', async (req, res, next) => {
+    if (req.query.revisionNumber) {
+        try {
+            const allItemsByVersion = await VolunteerItem.find({
+                revisionNumber: req.query.revisionNumber
+            });
+            res.json(allItemsByVersion);
+        } catch (err) {
+            next(err);
+        }
+    } else {
+        try {
+            const allItemsByMostRecentVersion = await VolunteerItem.aggregate(
+                [
+                    // Group by revisionNumber, assembling an array of docs with each distinct value.
+                    {
+                        $group: {
+                            _id: '$revisionNumber',
+                            items: { $push: '$$ROOT' }
+                        }
+                    },
+                    // Sort the groups by _id descending to put the max revisionNumber group first.
+                    { $sort: { revisionNumber: -1 } },
+                    // Return just the first (max revisionNumber) group of docs.
+                    { $limit: 1 }
+                ]
+            );
+            res.json(allItemsByMostRecentVersion);
+        } catch (err) {
+            next(err);
+        }
+    }
+});
+
+router.get('/getTransaction', async (req, res, next) => {
     const { startDate, endDate, transactionType } = req.query;
 
     let query = {};
@@ -65,14 +122,34 @@ router.post('/updateItems', async (req, res, next) => {
     const { updatedItems, itemType } = req.body;
 
     if (itemType === 'SHOP') {
+        for (let i = 0; i < updatedItems.length; i++) {
+            updatedItems[i].revisionNumber++;
+        }
 
+        ShopItem.create(updatedItems, (err) => {
+            if (err) {
+                res.status(500).send(err);
+                return;
+            }
+
+            res.status(200).send("Success");
+        });
     } else if (itemType === 'VOLUNTEER') {
+        for (let i = 0; i < updatedItems.length; i++) {
+            updatedItems[i].revisionNumber++;
+        }
 
+        VolunteerItem.create(updatedItems, (err) => {
+            if (err) {
+                res.status(500).send(err);
+                return;
+            }
+
+            res.status(200).send("Success");
+        });
     } else {
         res.status(500).send("Invalid item type: should be either SHOP or VOLUNTEER.");
     }
-
-
 });
 
 module.exports = router;
