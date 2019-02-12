4b25e078fb35364893bee2bab6973a7b6fa9c6fa
Sukhmai Kapur <39801345+Sukhmai@users.noreply.github.com>
2019-02-12 16:05:00 -0500

    testing adding signup

---
+++ frontend/src/models/SignupModel.js
@@ -0,0 +1,22 @@
+import { observable } from 'mobx';
+
+import api from '../../assets/js/api';
+
+export default class SignupModel {
+  @observable user;
+  @observable state;
+  @observable onSignup;
+
+  signup(username, password) {
+    this.state = 'pending';
+    api.post('/api/signup', { email: username, password})
+      .then((res) => {
+        if (res && res.setCookies && res.setCookies.token) {
+          this.user = res.setCookies.token;
+          console.log("user", this.user);
+        }
+        this.state = 'success'
+      })
+      .catch(() => this.state = 'error');
+  }
+}
