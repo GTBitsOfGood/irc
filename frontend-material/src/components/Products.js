7808f01c3518dc9f2e8e36b335ef55abc42bde5a
Sukhmai Kapur <39801345+Sukhmai@users.noreply.github.com>
2019-04-06 19:19:18 -0400

    fixed id bug

--- frontend-material/src/components/Products.js
+++ frontend-material/src/components/Products.js
@@ -30,7 +30,7 @@ class Products extends Component {
             key={product.id}
             price={product.price}
             name={product.name}
-            id={product.id}
+            id={product._id}
             addToCart={this.props.addToCart}
             productQuantity={this.props.productQuantity}
             updateQuantity={this.props.updateQuantity}
