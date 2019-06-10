a40b91f14a1a019a665739e5609c3769a4101c7c
Sukhmai Kapur <39801345+Sukhmai@users.noreply.github.com>
2019-06-10 13:04:01 -0400

    Added Reset Password Route'

--- frontend-material/src/views/UserProfile/UserProfile.jsx
+++ frontend-material/src/views/UserProfile/UserProfile.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 // @material-ui/core components
 import withStyles from "@material-ui/core/styles/withStyles";
 // core components
@@ -10,6 +10,9 @@ import Card from "components/Card/Card.jsx";
 import CardHeader from "components/Card/CardHeader.jsx";
 import CardBody from "components/Card/CardBody.jsx";
 import CardFooter from "components/Card/CardFooter.jsx";
+
+import {callBackendAPI} from "components/CallBackendApi";
+import ErrorDialog from "components/ErrorDialog";
 
 const styles = {
   cardCategoryWhite: {
@@ -30,46 +33,102 @@ const styles = {
   }
 };
 
-function UserProfile(props) {
-  const { classes } = props;
-  return (
-    <div>
-        <GridItem xs={12} sm={12} md={12}>
-          <Card>
-            <CardHeader color="primary">
-              <h4 className={classes.cardTitleWhite}>Edit Profile</h4>
-              <p className={classes.cardCategoryWhite}>Update password here</p>
-            </CardHeader>
-            <CardBody>
-              <GridContainer>
-                <GridItem xs={12} sm={12} md={6}>
-                  <CustomInput
-                    labelText="New password"
-                    id="first-name"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
-                  />
-                </GridItem>
-                <GridItem xs={12} sm={12} md={6}>
-                  <CustomInput
-                    labelText="Confirm"
-                    id="last-name"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
-                  />
-                </GridItem>
-              </GridContainer>
-            </CardBody>
-            <CardFooter>
-              <Button color="primary">Reset Password</Button>
-              <Button color="primary">Update Profile</Button>
-            </CardFooter>
-          </Card>
-        </GridItem>
-    </div>
-  );
+
+
+class UserProfile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      password: "",
+      confirm: "",
+      open: false
+    }
+
+    this.handleResetPassword = this.handleResetPassword.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  handlePasswordChange = event => {
+    this.setState({ password: event.target.value });
+  }
+
+  handleConfirmChange = event => {
+    this.setState({ confirm: event.target.value });
+  }
+
+  handleResetPassword() {
+    if (this.state.password === this.state.confirm) {
+      callBackendAPI('/api/changePassword', 'post', {
+        password: this.state.password
+      }).then(response => {
+        if (response) {console.log(response);}
+      })
+    } else {
+      this.setState({
+        open: true
+      });
+    }
+  }
+
+  handleClose() {
+    this.setState({
+      open: false
+    });
+  }
+
+  render() {
+    return (
+      <div>
+          <GridItem xs={12} sm={12} md={12}>
+            <Card>
+              <CardHeader color="primary">
+                <h4>Edit Profile</h4>
+                <p>Update password here</p>
+              </CardHeader>
+              <CardBody>
+                <GridContainer>
+                  <GridItem xs={12} sm={12} md={6}>
+                    <CustomInput
+                      labelText="New password"
+                      id="first-name"
+                      inputProps={{
+                        onChange:(e) => this.handlePasswordChange(e),
+                        type: "password"
+                      }}
+                      formControlProps={{
+                        fullWidth: true
+                      }}
+                    />
+                  </GridItem>
+                  <GridItem xs={12} sm={12} md={6}>
+                    <CustomInput
+                      labelText="Confirm"
+                      id="last-name"
+                      inputProps={{
+                        onChange:(e) => this.handleConfirmChange(e),
+                        type: "password"
+                      }}
+                      formControlProps={{
+                        fullWidth: true
+                      }}
+                    />
+                  </GridItem>
+                </GridContainer>
+              </CardBody>
+              <CardFooter>
+                <Button onClick={() => this.handleResetPassword()} color="primary">Reset Password</Button>
+              </CardFooter>
+            </Card>
+          </GridItem>
+          <ErrorDialog
+            open = {this.state.open}
+            handleClose = {this.handleClose}
+            message = {"Your passwords don't match!"}
+          />
+      </div>
+    );
+  }
 }
 
 export default withStyles(styles)(UserProfile);
