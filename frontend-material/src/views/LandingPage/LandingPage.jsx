78239e6eb4438cca4763bd7b54a3ccba10c29876
Michael Chen <michael.ld.chen@gmail.com>
2019-09-18 21:38:09 -0400

    fix typo I made in Error 401 check

--- frontend-material/src/views/LandingPage/LandingPage.jsx
+++ frontend-material/src/views/LandingPage/LandingPage.jsx
@@ -36,7 +36,7 @@ class LandingPage extends React.Component {
 
   componentWillMount() {
     callBackendAPI('/api/verify', 'post', {}).then(response => {
-      if (response.error === "Error 401 - Unauthorized - No login token provided") {
+      if (response.error === "Error 401 - Unauthorized - No login token  provided") {
         this.setState({
           isNotLoggedIn: true
         });
@@ -97,86 +97,86 @@ class LandingPage extends React.Component {
   handleChangeIndex = index => {
     this.setState({ value: index });
   };
+
   render() {
     const { classes } = this.props;
     if (this.state.redirect !== false) {
-      return <Redirect to={this.state.redirect} />
+      return <Redirect to={this.state.redirect} />;
     }
 
     return (
-    <div>
-    {!this.state.isNotLoggedIn &&
-      <center>
-        <Card style={{ width: "60%" }}>
-          <CardHeader color="primary">
-            <h4 className={classes.cardTitleWhite}> <center> Welcome, {this.state.username} </center></h4>
-          </CardHeader>
-          <CardBody>
-            <p> Please select an action on the left or log out below. </p>
-          </CardBody>
-        </Card>
-      </center>
-    }
       <div>
-        {this.state.isNotLoggedIn &&
+        {!this.state.isNotLoggedIn &&
+          <center>
+            <Card style={{ width: "60%" }}>
+              <CardHeader color="primary">
+                <h4 className={classes.cardTitleWhite}><center> Welcome, {this.state.username} </center></h4>
+              </CardHeader>
+              <CardBody>
+                <p> Please select an action on the left or log out below. </p>
+              </CardBody>
+            </Card>
+          </center>
+        }
+        <div>
+          {this.state.isNotLoggedIn &&
+            <center>
+            <Card style={{ width: "20rem" }}>
+              <CardBody>
+                <CardHeader style={{ width: "10rem", bottom:"1rem" }} color="info">
+                  <h4><center> Login/Signup </center></h4>
+                </CardHeader>
+                <TextField
+                  label="Username"
+                  margin="normal"
+                  onChange={e => this.setState({ username: e.target.value })}
+                />
+                <TextField
+                  margin="normal"
+                  label="Password"
+                  inputProps={{type: "password"}}
+                  onChange={e => this.setState({ password: e.target.value })}
+                />
+              </CardBody>
+              <CardBody>
+                <Button
+                  style={{ width: "5rem", right:"1rem" }}
+                  color="success"
+                  round={true}
+                  onClick={() => this.handleLogin()}
+                >
+                  Login
+                </Button>
+                <Button
+                  style={{ width:"5rem", left:"1rem" }}
+                  color="success"
+                  round={true}
+                  onClick={() => this.handleSignup()}
+                >
+                  Sign Up
+                </Button>
+              </CardBody>
+            </Card>
+            </center>
+          }
+        </div>
+        {!this.state.isNotLoggedIn &&
           <center>
           <Card style={{ width: "20rem" }}>
-            <CardBody >
-              <CardHeader style={{ width: "10rem", bottom:"1rem" }} color="info">
-                <h4> <center> Login/Signup </center></h4>
-              </CardHeader>
-              <TextField
-                label="Username"
-                margin="normal"
-                onChange={e => this.setState({ username: e.target.value })}
-              />
-              <TextField
-                margin="normal"
-                label="Password"
-                inputProps={{type: "password"}}
-                onChange={e => this.setState({ password: e.target.value })}
-              />
+            <CardBody>
+              <center>
+                <Button color="danger"> Log out </Button>
+              </center>
             </CardBody>
-              <CardBody>
-              <Button
-                style={{ width: "5rem", right:"1rem" }}
-                color="success"
-                onClick={() => this.handleLogin()}
-                round={true}
-                type="submit"
-              >
-                Login
-              </Button>
-              <Button
-                style={{ width:"5rem", left:"1rem" }}
-                color="success"
-                round="true"
-                onClick={() => this.handleSignup()}
-              >
-                Sign Up
-              </Button>
-              </CardBody>
           </Card>
           </center>
         }
+        <ErrorDialog
+          open = {this.state.open}
+          handleClose = {this.handleClose}
+          message = {this.state.message}
+        />
       </div>
-      {!this.state.isNotLoggedIn &&
-        <center>
-        <Card style={{ width: "20rem" }}>
-          <CardBody>
-            <center>
-              <Button color="danger">Log out</Button>
-            </center>
-          </CardBody>
-        </Card>
-        </center>
-      }
-      <ErrorDialog
-        open = {this.state.open}
-        handleClose = {this.handleClose}
-        message = {this.state.message}
-      />
-    </div>
     );
   }
 }
