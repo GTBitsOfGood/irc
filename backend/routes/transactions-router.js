9f60869460d8053b155bd2e925804579524ac210
Bryce Plunkett <plunkett.bryce.m@gmail.com>
2019-10-06 21:01:22 -0400

    fixed small bug

--- backend/routes/transactions-router.js
+++ backend/routes/transactions-router.js
@@ -107,13 +107,14 @@ router.get('/getTransaction', async (req, res, next) => {
 });
 
 router.post('/updateItems', async (req, res, next) => {
+    const { updatedItems, itemType } = req.body;
+
     let maxRevisionNumber;
     if (itemType === 'SHOP') {
         maxRevisionNumber = await ShopItem.getMostRecentRevisionNumber();
-    } else {
+    } else if (itemType === 'VOLUNTEER') {
         maxRevisionNumber = await VolunteerItem.getMostRecentRevisionNumber();
     }
-    const { updatedItems, itemType } = req.body;
     for (let i = 0; i < updatedItems.length; i++) {
         const item = updatedItems[i];
         item.revisionNumber = maxRevisionNumber + 1;
