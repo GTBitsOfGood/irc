374d82f83384185ac70f06b35f7a84a87fc5fa24
Bryce Plunkett <plunkett.bryce.m@gmail.com>
2019-10-08 19:40:24 -0400

    new client routes

---
+++ backend/routes/client-router.js
@@ -0,0 +1,39 @@
+const RESPONSE = require('../utilities/response');
+
+const express = require('express');
+const router = express.Router();
+const clientModel = require('../model/client');
+
+router.post('/addClient', async (req, res, next) => {
+    const client = req.body;
+    clientModel.create(client, (err) => {
+        if (!!err) {
+            res.json(RESPONSE.generateResponseMessage('Error 400 - Bad Request- ' +
+                'Invalid syntax when trying to' +
+                ' create client (see error)', 400, error = err));
+        } else {
+            res.json(RESPONSE.generateOkResponse("Client added successfully"));
+        }
+    });
+});
+
+router.get('/:id?', async (req, res, next) => {
+    try {
+        const clientID = req.params.id;
+
+        let filter;
+        if (clientID == null) {
+            filter = {};
+        } else {
+            filter =  {caseNumber: clientID};
+        }
+        const output = await clientModel.find(filter);
+        res.json(RESPONSE.generateOkResponse("All is well.", output));
+    } catch (err) {
+        res.json(RESPONSE.generateInternalServerError(err));
+    }
+});
+
+
+
+module.exports = router;
