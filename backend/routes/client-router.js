2103742cf9cc9d8dd9ee7a6c292faee498dec641
Bryce Plunkett <plunkett.bryce.m@gmail.com>
2019-10-10 14:56:14 -0400

    add an 'updateClient' route and validation for 'addClient' route

--- backend/routes/client-router.js
+++ backend/routes/client-router.js
@@ -4,20 +4,39 @@ const express = require('express');
 const router = express.Router();
 const clientModel = require('../model/client');
 
-router.post('/addClient', async (req, res, next) => {
+router.post('/addClient', async (req, res) => {
     const client = req.body;
-    clientModel.create(client, (err) => {
-        if (!!err) {
-            res.json(RESPONSE.generateResponseMessage('Error 400 - Bad Request- ' +
-                'Invalid syntax when trying to' +
-                ' create client (see error)', 400, error = err));
-        } else {
-            res.json(RESPONSE.generateOkResponse("Client added successfully"));
-        }
-    });
+    if (client.caseNumber && await clientModel.clientExists(client.caseNumber)) {
+        res.json(RESPONSE.generateParameterError({caseNumber: 'A client already exists with this case number'}));
+    } else {
+        clientModel.create(client, (err, newClient) => {
+            if (!!err) {
+                res.json(RESPONSE.generateResponseMessage('Error 400 - Bad Request- ' +
+                    'Invalid syntax when trying to' +
+                    ' create client (see error)', 400, error = err));
+            } else {
+                res.json(RESPONSE.generateOkResponse("Client added successfully", newClient));
+            }
+        });
+    }
 });
 
-router.get('/:id?', async (req, res, next) => {
+router.post('/updateClient', async (req, res) => {
+    const updateInfo = req.body;
+    if (updateInfo.caseNumber && await clientModel.clientExists(updateInfo.caseNumber)) {
+        const existingClient = await clientModel.findOne({caseNumber: updateInfo.caseNumber});
+        Object.keys(updateInfo).forEach((key) => {
+           existingClient[key] = updateInfo[key];
+        });
+        existingClient.save();
+        res.json(RESPONSE.generateOkResponse("Client updated successfully", existingClient));
+    } else {
+        res.json(RESPONSE.generateParameterError({caseNumber: 'Either the case number ' +
+                'you gave was null or a client with that case number already exists'}));
+    }
+});
+
+router.get('/:id?', async (req, res) => {
     try {
         const clientID = req.params.id;
 
